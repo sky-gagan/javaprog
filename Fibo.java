4332c3dc9f55371504615f6c110b3a2873d63d3d
Gagandeep Singh <gagandeepsangral@gmail.com>
2020-02-03 10:05:21 +0530

    2

--- Fibo.java
+++ Fibo.java
@@ -5,12 +5,19 @@ class Fibo
 	 int n = Integer.parseInt(args[0]);
          int i = 0;
          int a = 0,b=1,c=0;
-	 for(i=0;i<n;i++)
+	 if(n==1)
+	 System.out.println(a);
+	 if(n>=2)
+	{
+	 System.out.println(a +"\n"+b);
+        
+	 for(i=0;i<n-2;i++)
 	 {
 	  c = a + b;
 	  System.out.println(c);
 	  a = b;
 	  b = c;
+	  }
 	 }
 	}
 }
